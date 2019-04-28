b7cc855ee9fba05b935659d1b83b52575b748d17
Aisha Sharif <sharif_aisha@yahoo.com>
2019-04-28 15:35:04 +0300

    Add logic to submit quote form

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,7 +10,11 @@ export class QuoteComponent implements OnInit {
   quotes = [
     new Quote(1, "jane doe", "Robert A. Heinlein", "They didn't want it good, they wanted it Wednesday")
   ]
+  addNewQuote(quote){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength + 1;
+    this.quotes.push(quote);
+  }
   constructor() { }
   ngOnInit() { }
-
 }
